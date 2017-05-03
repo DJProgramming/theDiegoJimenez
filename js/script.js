d241847e9ba0c0991c3cf8ff3367bc777b623f7b
DJDeezy <diegojimenez1023@yahoo.com>
2017-05-03 15:52:09 -0700

    added video and photo toggles

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@ var main = function() {
   $('.menuIcon').click(function() {
     $('#navigation').fadeToggle(1000);
   });
+
+  $('#videoToggle').click(function() {
+    $('.videos').fadeToggle(1000);
+  });
+
+  $('#photoToggle').click(function() {
+    $('.photos').fadeToggle(1000);
+  });
 };
 
 $(document).ready(main);
