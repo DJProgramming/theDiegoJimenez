cbf0769ebdb691dfba3f296ddf27132ed925f581
DJDeezy <diegojimenez1023@yahoo.com>
2017-05-03 17:05:01 -0700

    changed video and photo toggles to divs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 var main = function() {
+  menu();           // show or hide menu
+  toggleVideos();   // show or hide videos
+  togglePhotos();   // show or hide photos
+};
+
+var menu = function() {
   $('.menuIcon').click(function() {
     $('#navigation').fadeToggle(1000);
   });
+};
 
+var toggleVideos = function() {
   $('#videoToggle').click(function() {
     $('.videos').fadeToggle(1000);
   });
+}
 
+var togglePhotos = function() {
   $('#photoToggle').click(function() {
     $('.photos').fadeToggle(1000);
   });
