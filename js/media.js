e7787573efe2a0e993376fb733a085db7b4ec7f2
DJDeezy <diegojimenez1023@yahoo.com>
2017-05-29 16:27:37 -0700

    completed photo navigation

--- js/media.js
+++ js/media.js
@@ -1,27 +1,39 @@
 var photoSlides;
+var lastPhoto = 0;
 
 var main = function() {
   photoSlides = document.getElementsByClassName("photoSlide");
   displayViewer();
+  navigate();
 }
 
 var displayViewer = function(number) {
 
-  $(".photo").click(function() {  // show viewer
+  $(".photoRow .photo").click(function() {          // show viewer
     $(".viewer").show();
-    for(var i = 0; i < photoSlides.length; i++) {
-      $(photoSlides[i]).hide();
-    }
-    $(photoSlides[number]).show();
-    // $(".photoSlide").show();
+    displayPhoto(number);
   });
-  $(".viewer").click(function() { // hide viewer
+  $(".close").click(function() {          // hide viewer
     $(".viewer").hide();
   });
 }
 
-var getPhotoSlides = function() {
+var displayPhoto = function(number) {
+  $(photoSlides[lastPhoto]).hide();
+  $(photoSlides[number]).show();
+  lastPhoto = number;
+}
 
+// navigate to next or previous photo
+var navigate = function() {
+  $(".next").click(function() {
+    if(lastPhoto+1 < photoSlides.length)  // make sure there is a next photo
+      displayPhoto(lastPhoto+1);
+  });
+  $(".previous").click(function() {
+    if(lastPhoto-1 >= 0)                  // make sure there is a previous photo
+      displayPhoto(lastPhoto-1);
+  });
 }
 
 $(document).ready(main);
